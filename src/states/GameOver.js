6ccb4bc637a0451e673d3cb2818b8752cc94717c
Gabriel Pedrosa <gabriel.pedrosa@waycarbon.com>
2017-05-10 00:41:45 -0300

    implementada a exibição de perguntas + suas regras de negócio, tela de fim de jogo com botão para voltar ao inicio

--- src/states/GameOver.js
+++ src/states/GameOver.js
@@ -2,11 +2,22 @@
 class GameOver extends Phaser.State {
 
 	create() {
+    this.title_one = this.game.add.text(this.game.world.centerX, 750, "Fim do jogo", { font: "62px Arial Black", fill: "#000aff" });
+    this.title_one.anchor.setTo(0.5);
+    this.title_one.stroke = "#8877de";
+    this.title_one.strokeThickness = 16;
 
+    this.title_two = this.game.add.text(this.game.world.centerX, 1100, "Recomeçar?", { font: "62px Arial Black", fill: "#000aff" });
+    this.title_two.anchor.setTo(0.5);
+    this.title_two.stroke = "#8877de";
+    this.title_two.strokeThickness = 16;
+
+    this.title_two.inputEnabled = true; 
+    this.title_two.events.onInputDown.add(this.restartGame, this);
 	}
 
 	restartGame() {
-		this.game.state.start("Main");
+		this.game.state.start("GameTitle");
 	}
 
 }
--- src/states/Main.js
+++ src/states/Main.js
@@ -3,12 +3,118 @@ import ExampleObject from 'objects/ExampleObject';
 
 class Main extends Phaser.State {
 
+  constructor () {
+    super();
+  }
+
 	create() {
 		this.game.stage.backgroundColor = '#cecece';
+    this.getQuestions();
+    this.currentIndex = 0;
+    this.screenElements = []; // so that they can be removed when changing questions
+    this.showCurrentQuestion();
 	}
 
 	update() {
+
 	}
+
+  // create boxes and texts
+  showCurrentQuestion () {
+    
+    // clear screen from previous answer texts
+    this.emptyScreen();
+
+    var x = 20;
+    var questionY = 40;
+    var answerY = 400;
+    var answerPadding = 50;
+    var questionStyle = {
+      font: "56px Arial", fill: '#ffffff', backgroundColor: 'rgba(0,255,0,0.25)',
+      align: 'left', wordWrap: true, wordWrapWidth: 1000      
+    }
+    var answerStyle = {
+      font: "56px Arial", fill: '#ffffff', backgroundColor: 'rgba(0,255,0,0.25)',
+      align: 'left', wordWrap: true, wordWrapWidth: 1000       
+    }
+
+    // question object
+    var question = this.questions[this.currentIndex];
+
+    // question text
+    var questionText = this.game.add.text(x, questionY, question.pergunta,  questionStyle);
+    this.screenElements.push(questionText);
+
+    this.shuffleAnswers(question.respostas);
+
+    // print alternatives on screen
+    for (var i in question.respostas) {
+      var obj = question.respostas[i];
+      var answerText = this.game.add.text(x, answerY, obj.resposta,  answerStyle);
+      var callback = obj.certa ? this.correctAnswer : this.wrongAnswer;
+      answerText.inputEnabled = true; 
+      answerText.events.onInputDown.add(callback, this);
+      this.screenElements.push(answerText);
+
+      // adjust height of next alternative
+      answerY += answerPadding + answerText.height;
+    }
+  }
+
+  shuffleAnswers (array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  // clear screen
+  emptyScreen () {
+    for (var i in this.screenElements) {
+      this.screenElements[i].destroy();
+    }
+    this.screenElements = [];
+  }
+
+  correctAnswer () {
+    alert('Resposta correta! :D');
+    this.showNextQuestion();
+  }
+
+  wrongAnswer () {
+    alert('Resposta errada... :(');
+    this.game.state.start("GameOver");
+  }
+
+  showNextQuestion () {
+    this.currentIndex++;
+    if (this.currentIndex >= this.questions.length) {
+      this.game.state.start("GameOver");
+    } else {
+      this.showCurrentQuestion();
+    }
+  }
+
+  getQuestions () {
+    this.questions = this.game.cache.getJSON('questions').perguntas;
+    this.questions.sort(function (a, b) {
+      if (a.dificuldade > b.dificuldade) return 1;
+      if (a.dificuldade < b.dificuldade) return -1;
+      if (a.dificuldade == b.dificuldade) return 0;
+    });
+  }
 
 }
 
